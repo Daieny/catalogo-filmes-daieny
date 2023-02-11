7ea097f23a459059976753e4cce1fda3fe47fa64
Daieny <daieny@icloud.com>
2023-02-11 16:55:00 -0300

    Atualizando a services

--- app/src/main/kotlin/br/com/catalogo/services/ActorServices.kt
+++ app/src/main/kotlin/br/com/catalogo/services/ActorServices.kt
@@ -9,38 +9,13 @@ private operator fun Char.times(index: Int) {
 
 }
 
-@ActorServices.Current
-class ActorServices(actor: Actor) {
-    annotation class Current
 
-    fun save(actor: Actor) {
-        val lastUpdate = LocalDateTime.now()
-        val rodrigoLombardi = Actor(
-                1,
-                "Rodrigo",
-                "Lombardi",
-                LocalDateTime.now()
-        )
+class ActorServices{
 
-        val dricaMoraes = Actor(
-                2,
-                "Drica",
-                "Moraes",
-                LocalDateTime.now()
+     val id: Int,
+     val firstName: String,
 
-
-        )
-        val listOfActor = listOf(TODO())
-
-        val map = listOfActor.mapIndexed { index, it ->
-            it * index
-        }
-        println(map)
-        val id: Int,
-        val firstName
-        val lasName
-        val actorQuery
-
+    fun save(actor: Actor){
 
         if (actor.id < 0){
         throw Exception("Id inválido")
@@ -55,10 +30,13 @@ class ActorServices(actor: Actor) {
 
     fun actorValidate(actor: Actor) {
 
+
     }
 
     fun getAll() = listOf("Rodrigo Lombardi", "Drika Moraes", "Bruna Marquezine", "Camila Queiroz", "Agatha Moreira")
 
 }
-
+    fun getById(actor: Actor){
+        id: Int -> actor_id: Long Actor? = repository.findById(id).get()
+    }
 }
